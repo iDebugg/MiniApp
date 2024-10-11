b4d182cb01a661c3e75aad7c5a382eacdd4b6099
ADEMOLA <mac@Ademola-MacBook-Pro.local>
2024-10-11 19:58:16 +0400

    new adsd

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "../Styles/Loader.css"
+import MASCOT from "../assets/img/MASCOT NORMAL.png"
 
 function Loader() {
   return (
     <div className='loadingScreen'>
- 
+      <h5>SoftNote ™ </h5>
+      <h4>-Rush By TECTUM-</h4>
+
+      <img src={MASCOT} alt="" />
     </div>
   )
 }
